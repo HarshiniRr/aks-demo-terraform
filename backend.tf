bb9de2a2eafd04f25010e143f3616e69f8fa3415
HarshiniRr <52816212+HarshiniRr@users.noreply.github.com>
2021-02-25 11:39:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
     backend "azurerm" {
     
-        resource_group_name   = "terraform-storage-rg"
-        storage_account_name  = "terraformstatexlrwdrzss"
-        container_name        = "tfstatefiles"
-        access_key            = "E8sY6t6VhC0A7d647K94Sj7cDFUVHAlvlBtxfV8GrmX9lOAIpGbNcl3cHRzYUAOYPvvdYY8RwqtEmdlxGxME9w=="
-        key                   = "terraform.tfstate"
+       # resource_group_name   = "terraform-storage-rg"
+       # storage_account_name  = "terraformstatexlrwdrzss"
+       # container_name        = "tfstatefiles"
+       # access_key            = "E8sY6t6VhC0A7d647K94Sj7cDFUVHAlvlBtxfV8GrmX9lOAIpGbNcl3cHRzYUAOYPvvdYY8RwqtEmdlxGxME9w=="
+       # key                   = "terraform.tfstate"
 
 
     }
