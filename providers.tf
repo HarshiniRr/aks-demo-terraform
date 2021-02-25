16a64ac25486573b033398e1449137c3339599e7
HarshiniRr <52816212+HarshiniRr@users.noreply.github.com>
2021-02-25 11:55:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,7 +18,7 @@ backend "azurerm" {
     
         resource_group_name   = "terraform-storage-rg"
         storage_account_name  = "terraformstatexlrwdrzss"
-        container_name        = "tfstate"
+        container_name        = "tsfate1 "
         access_key            = "E8sY6t6VhC0A7d647K94Sj7cDFUVHAlvlBtxfV8GrmX9lOAIpGbNcl3cHRzYUAOYPvvdYY8RwqtEmdlxGxME9w=="
         key                   = "terraform.tfstate"
 
