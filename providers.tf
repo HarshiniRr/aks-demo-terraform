c416fdc3fa48d285e64c218c5c5ec5f2bc5ef1c4
HarshiniRr <52816212+HarshiniRr@users.noreply.github.com>
2021-02-25 11:47:35 +0530

    Update providers.tf
    added backend block to provider

--- providers.tf
+++ providers.tf
@@ -14,4 +14,15 @@ terraform {
       version = "2.39.0"
     }
   }
-}
+backend "azurerm" {
+    
+        resource_group_name   = "terraform-storage-rg"
+        storage_account_name  = "terraformstatexlrwdrzss"
+        container_name        = "tfstate"
+        access_key            = "E8sY6t6VhC0A7d647K94Sj7cDFUVHAlvlBtxfV8GrmX9lOAIpGbNcl3cHRzYUAOYPvvdYY8RwqtEmdlxGxME9w=="
+        key                   = "terraform.tfstate"
+
+
+    }
+}  
+
